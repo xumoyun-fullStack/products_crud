d92008387923ab4a7cbb2799af75a1f1bb16c1bd
xumoyun-fullStack <bobajanovxumoyun@gmail.com>
2022-05-21 04:59:35 +0500

    ready inshaalloh

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 router.get("/", ProductGet);
 router.post("/", ProductsPost);
-router.delete("/delete-product", DeleteProductPost)
+router.post("/delete-product", DeleteProductPost)
 router.post("/update-name", UpdateNamePost);
 router.post("/update-price", UpdatePricePost)
 
